1733518f3ff46f2b50d5af6ffea5cb8ca6b85e0f
Pigeon <pigeon@pigeond.net>
2007-08-13 23:27:29 +1000

    mpcam: height adjustment

--- fgmap_mpcam.js
+++ fgmap_mpcam.js
@@ -2,7 +2,7 @@
 
 var FGMPCAM_URL = 'http://pigeond.net:8001/mpcam';
 var FGMPCAM_WIDTH = 240;
-var FGMPCAM_HEIGHT = 180;
+var FGMPCAM_HEIGHT = 192;
 var FGMPCAM_CONTROL_URL = 'fg_mpcam_control.cgi';
 var FGMPCAM_POLL_INTERVAL = 2000;
 
