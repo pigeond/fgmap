e3f69abb7a128ce61775f061f7781eca46c730d4
Pigeon <pigeon@pigeond.net>
2009-11-20 22:34:37 +0800

    fgmap.js: more icon updates for heavyjet and atc

--- fgmap.js
+++ fgmap.js
@@ -136,7 +136,7 @@ var FGMAP_CRAFT_ICON_SMALLJET = "smalljet/smalljet";
 var FGMAP_CRAFT_MODELS_SMALLJET = [ "Citation-II", "Bravo", "fokker100", "tu154B" ];
 
 var FGMAP_CRAFT_ICON_HEAVYJET = "heavyjet/heavyjet";
-var FGMAP_CRAFT_MODELS_HEAVYJET = [ "boeing733", "boeing747-400-jw", "a320-fb", "A380", "AN-225-model", "B-52F-model", "Concorde-ba", "FINNAIRmd11", "MD11", "KLMmd11", "737-300", "787", "777-200", "747-400", "737-100" ];
+var FGMAP_CRAFT_MODELS_HEAVYJET = [ "boeing733", "boeing747-400-jw", "a320-fb", "A380", "AN-225-model", "B-52F-model", "Concorde-ba", "FINNAIRmd11", "MD11", "KLMmd11", "737-300", "787", "777-200", "747-400", "737-100", "737-400" ];
 
 
 var FGMAP_CRAFT_ICON_GLIDER = "glider/glider";
@@ -163,7 +163,7 @@ var FGMAP_CRAFT_ICON_E3B = "e3b/e3b";
 var FGMAP_CRAFT_MODELS_E3B = [ "E3B" ];
 
 var FGMAP_CRAFT_ICON_ATC = "atc/atc";
-var FGMAP_CRAFT_MODELS_ATC = [ "atc-tower", "mibs", "atc" ];
+var FGMAP_CRAFT_MODELS_ATC = [ "atc-tower", "atc-tower2", "mibs", "atc" ];
 
 var dummy_cnt = 1;
 
