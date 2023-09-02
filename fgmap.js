d12d6559acba47802c41aa6a7c2cc457fcb7e929
Pigeon <pigeon@pigeond.net>
2023-09-02 13:08:29 +1000

    fgmap.js: Update SINGLEPROP and GLIDER icons. Credit to @hbeni

--- fgmap.js
+++ fgmap.js
@@ -127,7 +127,7 @@ var FGMAP_CRAFT_ICON_HELI = "heli/heli"
 var FGMAP_CRAFT_MODELS_HELI = [ "bo105", "sikorsky76c", "ec135", "r22", "s76c", "Lynx-WG13", "S51-sikorsky", "CH47", "R22", "apache-model", "uh-1", "uh60", "OH-1", "ec130b4", "ec130t2" ];
 
 var FGMAP_CRAFT_ICON_SINGLEPROP = "singleprop/singleprop";
-var FGMAP_CRAFT_MODELS_SINGLEPROP = [ "c150", "c172p", "c172-dpm", "c182-dpm", "c310-dpm", "c310u3a", "dhc2floats", "pa28-161", "pc7", "j3cub" ];
+var FGMAP_CRAFT_MODELS_SINGLEPROP = [ "c150", "c172p", "c172-dpm", "c182-dpm", "c182s", "c182t", "c310-dpm", "c310u3a", "dhc2floats", "pa28-161", "pc7", "j3cub" ];
 
 var FGMAP_CRAFT_ICON_TWINPROP = "twinprop/twinprop";
 var FGMAP_CRAFT_MODELS_TWINPROP = [ "Boeing314Clipper", "Lockheed1049_twa", "TU-114-model", "b1900d-anim", "b29-model", "beech99-model", "dc3-dpm", "fokker50" ];
@@ -140,7 +140,7 @@ var FGMAP_CRAFT_MODELS_HEAVYJET = [ "boeing733", "boeing747-400-jw", "a320-fb", 
 
 
 var FGMAP_CRAFT_ICON_GLIDER = "glider/glider";
-var FGMAP_CRAFT_MODELS_GLIDER = [ "hgldr-cs-model", "paraglider_model", "colditz-model", "sgs233" ];
+var FGMAP_CRAFT_MODELS_GLIDER = [ "hgldr-cs-model", "paraglider_model", "colditz-model", "sgs233", "ask21", "ask21-jsb", "ask21mi", "ka6" ];
 
 var FGMAP_CRAFT_ICON_BLIMP = "blimp/blimp";
 var FGMAP_CRAFT_MODELS_BLIMP = [ "ZLT-NT", "ZF-balloon", "Submarine_Scout", "LZ-129", "Excelsior-model" ];
